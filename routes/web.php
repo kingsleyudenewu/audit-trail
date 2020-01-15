3ea4a0b77337d9430d8fa206a9e9ecc35f95f4d9
Udenewu Kingsley <kingzpacking@gmail.com>
2020-01-15 13:20:35 +0100

    Add route to project and place it in service provider

--- routes/web.php
+++ routes/web.php
@@ -1,1 +1,24 @@
 <?php
+
+use Illuminate\Support\Facades\Route;
+use Kingsleyudenewu\AuditTrail\Models\AuditTrail;
+
+Route::group(['prefix' => 'audit'], function () {
+    Route::get('/logs', function (){
+        $logs = AuditTrail::getAllLogs(25, true);
+        return response()->json([
+            'status' => true,
+            'message' => 'success',
+            'data' => $logs
+        ]);
+    });
+
+    Route::get('/logs/{id}', function ($id){
+        $user_log = AuditTrail::where('user_id', $id)->get();
+        return response()->json([
+            'status' => true,
+            'message' => 'success',
+            'data' => $user_log
+        ]);
+    });
+});
--- tests/AuditTrailTest.php
+++ tests/AuditTrailTest.php
@@ -2,6 +2,21 @@
 
 namespace Kingsleyudenewu\AuditTrail\Tests;
 
+use Kingsleyudenewu\AuditTrail\AuditTrailFacade;
+use Kingsleyudenewu\AuditTrail\AuditTrailServiceProvider;
+use Orchestra\Testbench\TestCase;
+
 class AuditTrailTest extends TestCase
 {
+    protected function getPackageProviders($app)
+    {
+        return [AuditTrailServiceProvider::class];
+    }
+
+    protected function getPackageAliases($app)
+    {
+        return [
+            'AuditTrail' => AuditTrailFacade::class
+        ];
+    }
 }
