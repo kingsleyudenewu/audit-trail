fee87048646f8f08cec6695dd5b9b4feb1d2e10a
Udenewu Kingsley <kingzpacking@gmail.com>
2020-01-15 11:17:03 +0100

    Apply fixes from StyleCI (#2)

--- database/migrations/2020_01_14_164512_create_audit_trail_table.php
+++ database/migrations/2020_01_14_164512_create_audit_trail_table.php
@@ -2,9 +2,9 @@
 
 namespace Kingsleyudenewu\AuditTrail\database\migrations;
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreateAuditTrailTable extends Migration
 {
