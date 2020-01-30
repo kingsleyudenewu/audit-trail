d7d0d8405a183832896e59cb00dbf36a2d1e13b7
Udenewu Kingsley <kingzpacking@gmail.com>
2020-01-30 12:20:56 +0100

    Apply fixes from StyleCI (#10)

--- tests/AuditTrailTest.php
+++ tests/AuditTrailTest.php
@@ -2,11 +2,11 @@
 
 namespace Kingsleyudenewu\AuditTrail\Tests;
 
+use Kingsleyudenewu\AuditTrail\AuditTrail as Audit;
 use Kingsleyudenewu\AuditTrail\AuditTrailFacade;
 use Kingsleyudenewu\AuditTrail\AuditTrailServiceProvider;
 use Kingsleyudenewu\AuditTrail\database\migrations\CreateAuditTrailTable;
 use Kingsleyudenewu\AuditTrail\Models\AuditTrail;
-use Kingsleyudenewu\AuditTrail\AuditTrail as Audit;
 use Orchestra\Testbench\TestCase;
 
 class AuditTrailTest extends TestCase
@@ -31,7 +31,7 @@ class AuditTrailTest extends TestCase
      */
     protected function getEnvironmentSetUp($app)
     {
-        include_once __DIR__ . '/../database/migrations/2020_01_24_151838_create_audit_trail_table.php';
+        include_once __DIR__.'/../database/migrations/2020_01_24_151838_create_audit_trail_table.php';
         (new CreateAuditTrailTable)->up();
     }
 
@@ -42,6 +42,7 @@ class AuditTrailTest extends TestCase
         $audit_trail->action = 'User logged in successfully';
         $audit_trail->model = "Kingsleyudenewu\AuditTrail\Models\AuditTrail";
         $audit_trail->save();
+
         return $audit_trail;
     }
 
@@ -59,19 +60,17 @@ class AuditTrailTest extends TestCase
     /**
      * @test
      */
-
     public function fetchAllLogs()
     {
         $this->createLog();
         $logs = new Audit();
-        $activity_array = $logs->getAllLogs(10,false);
+        $activity_array = $logs->getAllLogs(10, false);
         $this->assertNotEmpty($activity_array);
     }
 
     /**
      * @test
      */
-
     public function fetchSingleLog()
     {
         $this->withoutExceptionHandling();
